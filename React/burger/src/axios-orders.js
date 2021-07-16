ef3263ef2c022bd8811221e18859625822ebec2b
Daniel Ward <dward1502@gmail.com>
2021-07-15 22:16:25 -0700

    took out react and react native folder

--- React/burger/src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const instance = axios.create({
-    baseURL: 'https://burger-c72a3.firebaseio.com/'
-});
-
-export default instance;
--- React/redux-adv--01-start/src/store/actions/index.js
+++
@@ -1,2 +0,0 @@
-export{ add, subtract, increment, decrement} from './counter';
-export { storeResult, deleteResult } from './result';
